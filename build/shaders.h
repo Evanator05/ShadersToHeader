f169abcd7c3098a668cebff6747aa4e4212abd25
evanator05 <watsonevan2005@gmail.com>
2025-02-23 15:33:11 -0700

    corrected build folder example

--- build/shaders.h
+++ build/shaders.h
@@ -7,7 +7,7 @@ layout(rgba8, binding = 0) uniform image2D framebuffer;
 
 void main() {
     ivec2 pos = ivec2(gl_GlobalInvocationID.xy);
-    vec4 color = vec4(float(pos.x)/2550, float(pos.y)/1440, 0.0, 1.0);
+    vec4 color = vec4(0.0, 0.0, 1.0, 1.0);
     imageStore(framebuffer, pos, color);
 })";
 const char* greenCompute = R"(
@@ -18,7 +18,7 @@ layout(rgba8, binding = 0) uniform image2D framebuffer;
 
 void main() {
     ivec2 pos = ivec2(gl_GlobalInvocationID.xy);
-    vec4 color = vec4(float(pos.x)/2550, float(pos.y)/1440, 0.0, 1.0);
+    vec4 color = vec4(0.0, 1.0, 0.0, 1.0);
     imageStore(framebuffer, pos, color);
 })";
 const char* redCompute = R"(
@@ -29,7 +29,7 @@ layout(rgba8, binding = 0) uniform image2D framebuffer;
 
 void main() {
     ivec2 pos = ivec2(gl_GlobalInvocationID.xy);
-    vec4 color = vec4(float(pos.x)/2550, float(pos.y)/1440, 0.0, 1.0);
+    vec4 color = vec4(1.0, 0.0, 0.0, 1.0);
     imageStore(framebuffer, pos, color);
 })";
 }
--- build/shaders/greenCompute.glsl
+++ build/shaders/greenCompute.glsl
@@ -5,6 +5,6 @@ layout(rgba8, binding = 0) uniform image2D framebuffer;
 
 void main() {
     ivec2 pos = ivec2(gl_GlobalInvocationID.xy);
-    vec4 color = vec4(float(pos.x)/2550, float(pos.y)/1440, 0.0, 1.0);
+    vec4 color = vec4(0.0, 1.0, 0.0, 1.0);
     imageStore(framebuffer, pos, color);
 }
